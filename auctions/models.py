5bd4694126b083210d5f90a0719d3c3af4fe48d9
vuca1 <malihavita@gmail.com>
2026-01-30 12:30:38 +0100

    first commit

---
+++ auctions/models.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+class AuctionListings():
+    pass
+
+class Bids():
+    pass
+
+class Comments():
+    pass
